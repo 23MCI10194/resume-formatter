a2f3f2970ef5276b7b4ff17fda2b0d99eb342231
Sjmusic SJ MUSIC <sjmusic345@gmail.com>
2025-08-04 11:44:15 +0000

    no i need that logo only

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,19 +4,15 @@ export const Icons = {
   logo: (props: SVGProps<SVGSVGElement>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      viewBox="0 0 256 256"
+      fill="currentColor"
       {...props}
     >
-      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-      <polyline points="14 2 14 8 20 8" />
-      <line x1="16" y1="13" x2="8" y2="13" />
-      <line x1="16" y1="17" x2="8" y2="17" />
-      <polyline points="10 9 9 9 8 9" />
+      <rect width="256" height="256" fill="black" rx="32" ry="32"></rect>
+      <path
+        d="M164 68H92V88H112V168H92V188H164C186.091 188 204 170.091 204 148V108C204 85.9086 186.091 68 164 68ZM164 168H132V88H164C175.046 88 184 96.9543 184 108V148C184 159.046 175.046 168 164 168Z"
+        fill="white"
+      ></path>
     </svg>
   ),
 };
